97bfdc8dd89d7eedcaf9e3db58eae84d469f4867
Rudrapratap <rpdogra17@gmail.com>
2024-08-13 21:08:37 +0530

    Rudra_added_aad-row_Button

--- Js/script.js
+++ Js/script.js
@@ -67,3 +67,44 @@ function selectSalesmanSuggestion(name) {
     document.getElementById('salesman').value = name;
     document.getElementById('salesman-suggestions').style.display = 'none';
 }
+//add-row button added by Rudra
+document.getElementById('addRowBtn').addEventListener('click', addRow);
+function addRow() {
+ 
+    const tableBody = document.querySelector('table tbody');
+
+    const nextSerialNumber = tableBody.rows.length + 1;
+    const item = document.getElementById('ITEM').value || 'NONE';
+    const description = document.getElementById('DESCRIPTION').value || 'NONE';
+    const qty = document.getElementById('QTY').value || '';
+    const unit = document.getElementById('UNIT').value || '';
+    const pricePerUnit = document.getElementById('PRICE/UNIT').value || '';
+    const discount = document.getElementById('DISCOUNT').value || '';
+    const tax = document.getElementById('TAX').value || '';
+    const amount = document.getElementById('AMOUNT').value || '';
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td>${nextSerialNumber}</td>
+        <td>${item}</td>
+        <td>${description}</td>
+        <td>${qty}</td>
+        <td>${unit}</td>
+        <td>${pricePerUnit}</td>
+        <td>${discount}</td>
+        <td>${tax}</td>
+        <td>${amount}</td>
+    `;
+    tableBody.appendChild(newRow);
+    clearInputs();
+}
+
+function clearInputs() {
+    document.getElementById('ITEM').value = '';
+    document.getElementById('DESCRIPTION').value = '';
+    document.getElementById('QTY').value = '';
+    document.getElementById('UNIT').value = '';
+    document.getElementById('PRICE/UNIT').value = '';
+    document.getElementById('DISCOUNT').value = '';
+    document.getElementById('TAX').value = '';
+    document.getElementById('AMOUNT').value = '';
+}
